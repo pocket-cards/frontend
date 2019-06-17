c21190e0ba27bff80764d8a9e302c2bf8cb85a9a
wwalpha <wwalpha@gmail.com>
2019-06-17 10:04:21 +0900

    add amplify for next version

--- index.tsx
+++ index.tsx
@@ -7,26 +7,94 @@ import Router from './src/Router';
 import { register } from './src/serviceWorker';
 import theme from './src/Theme';
 import { MuiThemeProvider } from '@material-ui/core';
-
-const root = document.getElementById('root');
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
 
 // Amplify.configure({
 //   Auth: {
+//     // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+//     identityPoolId: 'ap-northeast-1:5b91c1fa-001a-479c-b57a-a91fac2520b6',
+
+//     // REQUIRED - Amazon Cognito Region
 //     region: 'ap-northeast-1',
-//     userPoolId: 'ap-northeast-1_GXUriRkpY',
-//     userPoolWebClientId: '4bp5nqfb6kp1ddk0moq4tl23a9',
+
+//     // OPTIONAL - Amazon Cognito User Pool ID
+//     userPoolId: 'ap-northeast-1_wddt9qPuu',
+
+//     // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+//     userPoolWebClientId: '3b0igerc2rmbqnfa50uf649rap',
+
+//     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+//     mandatorySignIn: false,
+
+//     // OPTIONAL - Configuration for cookie storage
+//     // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+//     cookieStorage: {
+//       // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+//       domain: '.aws-handson.com',
+//       // OPTIONAL - Cookie path
+//       path: '/',
+//       // OPTIONAL - Cookie expiration in days
+//       expires: 365,
+//       // OPTIONAL - Cookie secure flag
+//       // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+//       secure: true,
+//     },
 //   },
 // });
 
-render(
+const provider = (
   <Provider store={store}>
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
     </MuiThemeProvider>
-  </Provider>,
-  root,
+  </Provider>
 );
 
-register();
+const root = document.getElementById('root');
+
+const start = async () => {
+  // const result = await Auth.signUp({
+  //   username: 'wwalpha',
+  //   password: 'session10',
+  //   attributes: {
+  //     email: 'wwalpha@gmail.com',
+  //   },
+  // });
+
+  // const result = await Auth.confirmSignUp('wwalpha', '944880');
+
+  // const result = await Auth.signIn({
+  //   username: 'wwalpha',
+  //   password: 'session10',
+  // });
+
+  // console.log(result);
+  // After retrieving the confirmation code from the user
+  // Auth.confirmSignUp(username, code, {
+  //   // Optional. Force user confirmation irrespective of existing alias. By default set to True.
+  //   forceAliasCreation: true,
+  // })
+  //   .then(data => console.log(data))
+  //   .catch(err => console.log(err));
+
+  // Auth.resendSignUp(username)
+  //   .then(() => {
+  //     console.log('code resent successfully');
+  //   })
+  //   .catch(e => {
+  //     console.log(e);
+  //   });
+
+  // await Auth.signIn({
+  //   username: 'wwalpha',
+  //   password:
+  // })
+  render(provider, root);
+
+  register();
+};
+
+start();
--- typings/external.d.ts
+++ typings/external.d.ts
@@ -1,6 +1,8 @@
 // <reference path="../node_modules/@material-ui/core/index.d.ts" />
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
 /// <reference path="../node_modules/redux-thunk/index.d.ts" />
+/// <reference path="../node_modules/@aws-amplify/core/lib/index.d.ts" />
+/// <reference path="../node_modules/@aws-amplify/auth/lib/index.d.ts" />
 
 // <reference path="../node_modules/apollo-link-state/lib/index.d.ts" />
 // <reference path="../node_modules/react-apollo/index.d.ts" />
