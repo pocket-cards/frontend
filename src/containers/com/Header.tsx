985fa364606b866fd38d2b0eef0f62a6058e2ed0
wwalpha <wwalpha@gmail.com>
2019-06-20 10:49:33 +0900

    fix reload button

--- src/containers/com/Header.tsx
+++ src/containers/com/Header.tsx
@@ -15,19 +15,17 @@ class Header extends React.Component<Props, any, any> {
     return (
       <AppBar position="static" className={classes.app}>
         <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Reload"
-            onClick={() => {
-              // window.location.href = '/';
-              window.location.reload(true);
-            }}
-          >
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Reload">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.title}>
-            News
+            <Button
+              onClick={() => {
+                window.location.reload(true);
+              }}
+            >
+              Reload
+            </Button>
           </Typography>
           <Button color="inherit">Ver0.04</Button>
         </Toolbar>
