faf9024604f04ab477c5820b5ed453347fb750db
wwalpha <wwalpha@gmail.com>
2019-07-09 08:08:30 +0900

    version update

--- src/constants/Consts.ts
+++ src/constants/Consts.ts
@@ -4,6 +4,7 @@ export const API_NAME = 'api';
 
 export const A001_URL = (userId: string) => `/users/${userId}`;
 export const A002_URL = (userId: string) => `/users/${userId}/history`;
+export const A003_URL = (userId: string) => `/users/${userId}/fixdelay`;
 
 export const C001_URL = (groupId: string) => `/groups/${groupId}/words`;
 export const C004_URL = (groupId: string, word: string) => `/groups/${groupId}/words/${word}`;
@@ -15,11 +16,10 @@ export const C008_URL = (groupId: string) => `/groups/${groupId}/review`;
 export const D001_URL = () => '/image2text';
 
 export const GROUP_ID = 'x001';
-export const VERSION = '0.06';
+export const VERSION = '0.07';
 
 export const MODES = {
   New: '1',
-  // NewTest: '2',
   AllTest: '3',
   Review: '4',
 };
